7f0d60817e11bad6577dd2811a0f4d1fbd34feef
hamzagit786 <hsiddique0321@gmail.com>
2026-07-14 13:43:47 +0500

    Update rescue.js

--- rescue.js
+++ rescue.js
@@ -1,28 +1,23 @@
 const sourceDatabase = {
 
-TikTokShop: {
-USA: [
+TikTokShop:{
+USA:[
 ["Amazon.com","https://www.amazon.com"],
 ["Walmart.com","https://www.walmart.com"],
 ["eBay.com","https://www.ebay.com"],
 ["AliExpress","https://www.aliexpress.com"],
-["CJdropshipping","https://cjdropshipping.com"]
+["CJdropshipping","https://cjdropshipping.com"],
+["Alibaba","https://www.alibaba.com"]
 ],
 
 UK:[
 ["Amazon UK","https://www.amazon.co.uk"],
 ["eBay UK","https://www.ebay.co.uk"],
 ["Argos","https://www.argos.co.uk"],
+["Currys","https://www.currys.co.uk"],
+["Tesco","https://www.tesco.com"],
 ["AliExpress","https://www.aliexpress.com"],
 ["CJdropshipping","https://cjdropshipping.com"]
-],
-
-Germany:[
-["Amazon Germany","https://www.amazon.de"],
-["eBay Germany","https://www.ebay.de"],
-["Otto","https://www.otto.de"],
-["Kaufland","https://www.kaufland.de"],
-["AliExpress","https://www.aliexpress.com"]
 ],
 
 Canada:[
@@ -33,71 +28,267 @@ Canada:[
 ["CJdropshipping","https://cjdropshipping.com"]
 ],
 
-UAE:[
-["Amazon UAE","https://www.amazon.ae"],
-["Noon","https://www.noon.com"],
-["Carrefour UAE","https://www.carrefouruae.com"],
+Australia:[
+["Amazon Australia","https://www.amazon.com.au"],
+["eBay Australia","https://www.ebay.com.au"],
+["Kmart Australia","https://www.kmart.com.au"],
 ["AliExpress","https://www.aliexpress.com"],
 ["CJdropshipping","https://cjdropshipping.com"]
-]
-
-},
-
-
-Amazon:{
-USA:[
-["Amazon.com","https://www.amazon.com"],
-["Walmart.com","https://www.walmart.com"],
-["eBay.com","https://www.ebay.com"]
-],
-
-UK:[
-["Amazon UK","https://www.amazon.co.uk"],
-["eBay UK","https://www.ebay.co.uk"],
-["Argos","https://www.argos.co.uk"]
 ],
 
 Germany:[
 ["Amazon Germany","https://www.amazon.de"],
+["eBay Germany","https://www.ebay.de"],
 ["Otto","https://www.otto.de"],
-["Kaufland","https://www.kaufland.de"]
+["Kaufland","https://www.kaufland.de"],
+["MediaMarkt","https://www.mediamarkt.de"],
+["AliExpress","https://www.aliexpress.com"]
+],
+
+France:[
+["Amazon France","https://www.amazon.fr"],
+["Cdiscount","https://www.cdiscount.com"],
+["Fnac","https://www.fnac.com"],
+["AliExpress","https://www.aliexpress.com"],
+["CJdropshipping","https://cjdropshipping.com"]
+],
+
+Italy:[
+["Amazon Italy","https://www.amazon.it"],
+["eBay Italy","https://www.ebay.it"],
+["AliExpress","https://www.aliexpress.com"],
+["CJdropshipping","https://cjdropshipping.com"]
+],
+
+Spain:[
+["Amazon Spain","https://www.amazon.es"],
+["eBay Spain","https://www.ebay.es"],
+["AliExpress","https://www.aliexpress.com"],
+["CJdropshipping","https://cjdropshipping.com"]
+],
+
+Netherlands:[
+["Amazon Netherlands","https://www.amazon.nl"],
+["Bol.com","https://www.bol.com"],
+["AliExpress","https://www.aliexpress.com"],
+["CJdropshipping","https://cjdropshipping.com"]
+],
+
+Singapore:[
+["Amazon Singapore","https://www.amazon.sg"],
+["Shopee","https://shopee.sg"],
+["Lazada","https://www.lazada.sg"],
+["AliExpress","https://www.aliexpress.com"]
+],
+
+Malaysia:[
+["Shopee Malaysia","https://shopee.com.my"],
+["Lazada Malaysia","https://www.lazada.com.my"],
+["AliExpress","https://www.aliexpress.com"]
+],
+
+Thailand:[
+["Shopee Thailand","https://shopee.co.th"],
+["Lazada Thailand","https://www.lazada.co.th"],
+["AliExpress","https://www.aliexpress.com"]
+],
+
+Vietnam:[
+["Shopee Vietnam","https://shopee.vn"],
+["Lazada Vietnam","https://www.lazada.vn"],
+["AliExpress","https://www.aliexpress.com"]
+],
+
+Philippines:[
+["Shopee Philippines","https://shopee.ph"],
+["Lazada Philippines","https://www.lazada.com.ph"],
+["AliExpress","https://www.aliexpress.com"]
+],
+
+Indonesia:[
+["Tokopedia","https://www.tokopedia.com"],
+["Shopee Indonesia","https://shopee.co.id"],
+["Lazada Indonesia","https://www.lazada.co.id"]
+],
+
+UAE:[
+["Amazon UAE","https://www.amazon.ae"],
+["Noon","https://www.noon.com"],
+["Carrefour UAE","https://www.carrefouruae.com"],
+["AliExpress","https://www.aliexpress.com"]
+],
+
+SaudiArabia:[
+["Amazon Saudi","https://www.amazon.sa"],
+["Noon Saudi","https://www.noon.com"],
+["AliExpress","https://www.aliexpress.com"]
 ]
 
 },
 
-
-Shopify:{
+Amazon:{
 USA:[
-["AliExpress","https://www.aliexpress.com"],
-["Alibaba","https://www.alibaba.com"],
-["CJdropshipping","https://cjdropshipping.com"]
+["Walmart","https://www.walmart.com"],
+["eBay","https://www.ebay.com"],
+["Target","https://www.target.com"],
+["BestBuy","https://www.bestbuy.com"],
+["Costco","https://www.costco.com"]
 ],
 
 UK:[
-["AliExpress","https://www.aliexpress.com"],
-["Spocket","https://www.spocket.co"],
-["CJdropshipping","https://cjdropshipping.com"]
+["eBay UK","https://www.ebay.co.uk"],
+["Argos","https://www.argos.co.uk"],
+["Currys","https://www.currys.co.uk"],
+["Tesco","https://www.tesco.com"]
+],
+
+Germany:[
+["Otto","https://www.otto.de"],
+["Kaufland","https://www.kaufland.de"],
+["MediaMarkt","https://www.mediamarkt.de"]
 ]
 
 }
 
 };
+// Remaining Platform Sources
+
+Object.assign(sourceDatabase, {
+
+Shopify:{
+USA:[
+["AliExpress","https://www.aliexpress.com"],
+["Alibaba","https://www.alibaba.com"],
+["CJdropshipping","https://cjdropshipping.com"],
+["Spocket","https://www.spocket.co"],
+["Printful","https://www.printful.com"],
+["Printify","https://printify.com"],
+["Walmart","https://www.walmart.com"],
+["Amazon","https://www.amazon.com"]
+],
+
+UK:[
+["AliExpress","https://www.aliexpress.com"],
+["Alibaba","https://www.alibaba.com"],
+["CJdropshipping","https://cjdropshipping.com"],
+["Spocket","https://www.spocket.co"],
+["Printful","https://www.printful.com"],
+["Printify","https://printify.com"]
+],
+
+Canada:[
+["AliExpress","https://www.aliexpress.com"],
+["CJdropshipping","https://cjdropshipping.com"],
+["Spocket","https://www.spocket.co"],
+["Printful","https://www.printful.com"]
+],
+
+Australia:[
+["AliExpress","https://www.aliexpress.com"],
+["CJdropshipping","https://cjdropshipping.com"],
+["Printify","https://printify.com"]
+]
+
+},
+
+
+eBay:{
+USA:[
+["Amazon.com","https://www.amazon.com"],
+["Walmart","https://www.walmart.com"],
+["Target","https://www.target.com"],
+["Alibaba","https://www.alibaba.com"],
+["AliExpress","https://www.aliexpress.com"]
+],
+
+UK:[
+["Amazon UK","https://www.amazon.co.uk"],
+["Argos","https://www.argos.co.uk"],
+["Currys","https://www.currys.co.uk"],
+["Tesco","https://www.tesco.com"],
+["Alibaba","https://www.alibaba.com"]
+],
+
+Germany:[
+["Amazon Germany","https://www.amazon.de"],
+["Otto","https://www.otto.de"],
+["Kaufland","https://www.kaufland.de"],
+["MediaMarkt","https://www.mediamarkt.de"],
+["Alibaba","https://www.alibaba.com"]
+],
+
+France:[
+["Amazon France","https://www.amazon.fr"],
+["Fnac","https://www.fnac.com"],
+["Cdiscount","https://www.cdiscount.com"],
+["AliExpress","https://www.aliexpress.com"]
+]
+
+},
+
+
+Walmart:{
+USA:[
+["Amazon.com","https://www.amazon.com"],
+["Target","https://www.target.com"],
+["Best Buy","https://www.bestbuy.com"],
+["Costco","https://www.costco.com"],
+["Alibaba","https://www.alibaba.com"],
+["CJdropshipping","https://cjdropshipping.com"]
+],
+
+Canada:[
+["Amazon Canada","https://www.amazon.ca"],
+["Canadian Tire","https://www.canadiantire.ca"],
+["Alibaba","https://www.alibaba.com"],
+["AliExpress","https://www.aliexpress.com"]
+]
+
+},
+
+
+Etsy:{
+USA:[
+["Alibaba","https://www.alibaba.com"],
+["AliExpress","https://www.aliexpress.com"],
+["Printful","https://www.printful.com"],
+["Printify","https://printify.com"]
+],
+
+UK:[
+["Alibaba","https://www.alibaba.com"],
+["AliExpress","https://www.aliexpress.com"],
+["Printful","https://www.printful.com"],
+["Local Craft Suppliers",""]
+],
+
+Canada:[
+["Alibaba","https://www.alibaba.com"],
+["AliExpress","https://www.aliexpress.com"],
+["Printify","https://printify.com"]
+]
+
+}
+
+});
 
 
 
-
+// Load Sources Function
 
 function loadSources(){
 
-let platform = document.getElementById("sellingPlatform").value;
-let country = document.getElementById("targetCountry").value;
+let platform=document.getElementById("sellingPlatform").value;
 
-let results = document.getElementById("sourceResults");
+let country=document.getElementById("targetCountry").value;
+
+let results=document.getElementById("sourceResults");
+
 
 results.innerHTML="";
 
 
-let data = sourceDatabase[platform.replace(" ","")];
+let data=sourceDatabase[platform.replace(/\s/g,"")];
 
 
 if(data && data[country]){
@@ -115,8 +306,9 @@ results.innerHTML += `
 </h3>
 
 <p>
-Recommended source for ${country} market.
+Recommended source for ${country} ${platform} market.
 </p>
+
 
 <a class="btn" href="${item[1]}" target="_blank">
 Open Platform →
@@ -134,7 +326,7 @@ Open Platform →
 else{
 
 
-results.innerHTML = `
+results.innerHTML=`
 
 <div class="glass-card">
 
@@ -143,7 +335,7 @@ No recommendations available
 </h3>
 
 <p>
-More marketplaces will be added soon.
+More sources will be added soon.
 </p>
 
 </div>
@@ -157,13 +349,14 @@ More marketplaces will be added soon.
 
 
 
-
+// Product Finder
 
 function findProduct(){
 
 let name=document.getElementById("productName").value;
 
-if(name===""){
+
+if(name.trim()===""){
 
 alert("Please enter product name");
 
@@ -178,12 +371,11 @@ loadSources();
 
 
 
-
+// Dropdown Events
 
 document
 .getElementById("sellingPlatform")
 .addEventListener("change",loadSources);
-
 
 
 document
@@ -192,10 +384,10 @@ document
 
 
 
+// Default Load
 
-// Load default results
 window.onload=function(){
 
 loadSources();
 
-}
+};
